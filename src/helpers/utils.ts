e7ceb54b0485def79c333fde7b7399059046aa48
Mark <mark.korom88@gmail.com>
2023-02-08 13:38:28 +0100

    feat: add small code exercises

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,6 @@
 import { DataSource } from "typeorm";
+import { readdirSync } from "fs";
+import path from "path";
 
 export const disconnectAndClearDatabase = async (ds: DataSource): Promise<void> => {
   const { entityMetadatas } = ds;
@@ -6,3 +8,32 @@ export const disconnectAndClearDatabase = async (ds: DataSource): Promise<void> 
   await Promise.all(entityMetadatas.map(data => ds.query(`truncate table "${data.tableName}" cascade`)));
   await ds.destroy();
 };
+
+export function transformStringToNumberInArray(arr: string[]): (string | number)[] {
+  const mixedArr: (string | number)[] = [];
+  for (const element of arr) {
+    mixedArr.push(isNumeric(element) ? +element : element);
+  }
+  return mixedArr;
+}
+
+export function isStringContainsDigit(str: string): boolean {
+  return /\d/.test(str);
+}
+
+/**
+ * This solution is suggested here: @see: https://stackoverflow.com/a/175787/13319831
+ * Convert string to num in isNaN function, because ts complained about it.
+ * Do you know more elegant solution here?
+ * @param str
+ * @returns
+ */
+function isNumeric(str: string) {
+  return !isNaN(+str) && !isNaN(parseFloat(str));
+}
+
+type File = string;
+export function getSelectedFilesPathFromFolder(extension = "csv"): File[] {
+  // Folder path could be dynamic here. I think it is good enough for this short exercise.
+  return readdirSync(path.join(__dirname, "..", "..", "files")).filter(file => file.endsWith(extension));
+}
